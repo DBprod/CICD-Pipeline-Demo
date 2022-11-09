9c1a847d436349b80c5d121e80e37e250a8a7e8d
Ahmad Siddiqi <ahmad.siddiqi158@gmail.com>
2022-11-09 16:16:33 -0500

    configured heroku

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 def generate_buzz():
     page = '<html><body><h1>'
     page += generator.generate_buzz()
+    page += " ALSO DANIEL IS A BITCH"
     page += '</h1></body></html>'
     return page
 
