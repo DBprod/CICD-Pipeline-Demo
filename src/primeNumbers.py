e9e6cb8759f73ff1942558215ba50ff8abb200c3
Ahmad Siddiqi <ahmad.siddiqi158@gmail.com>
2022-11-27 16:06:16 -0500

    Cleaned up some code

--- src/primeNumbers.py
+++ src/primeNumbers.py
@@ -1,23 +1,19 @@
 def sumOfPrimes(n):
     if n <= 1:
         return 1
-
-    prime = [True] * (n + 1)  # list to store if number is prime numbers
-
+    prime = [True] * (n + 1) 
     p = 2
     while p * p <= n:
-        # if p is not changed in prime, then it is prime number
         if prime[p] == True:
-            i = p * 2  # update all multiples of p
+            i = p * 2  
 
             while i <= n:
                 prime[i] = False
                 i += p
         p += 1
-
-    totalSum = 0
-    for i in range(2, n + 1):  # find the sum by looking into prime list
+    tot = 0
+    for i in range(2, n + 1):  
         if prime[i]:
-            totalSum += i
-    return totalSum
+            tot += i
+    return tot
 		
